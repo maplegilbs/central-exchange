59f1742982dd29efa02e35d052ec041bfdf98f47
Scott Gilbert <scottvtgilbert@gmail.com>
2024-05-08 22:12:15 -0400

    Adding code for cron jobs for creekVTPhotosFetch

---
+++ crons/creekVTPhotosFetch.js
@@ -0,0 +1,14 @@
+//Libraries
+const path = require('path')
+//Functions
+const { getPhotosFromSpypointByCameraIDs } = require(path.join(process.cwd(),'/utils/creekvtcams/getPhotosFromSpypointAWS.js'))
+//Globals
+const { cameraListByOwner } = require (path.join(process.cwd(), '/utils/creekvtcams/cameraList.js'));
+
+//CRON job (to be run by heroku scheduler)
+for (let owner of Object.keys(cameraListByOwner)){
+    let curOwnerUserName = cameraListByOwner[owner].username;
+    let curOwnerPassword = cameraListByOwner[owner].password;
+    let curOwnerCameraIDs = cameraListByOwner[owner].cameras;
+    getPhotosFromSpypointByCameraIDs(curOwnerUserName, curOwnerPassword, curOwnerCameraIDs )
+}
