f44289d19c373324ac0e3d295345df431814a2e3
Scott Gilbert <scottvtgilbert@gmail.com>
2024-05-11 19:19:47 -0400

    Updating UTC time adjustment for use in heroku

--- utils/bearcobble/adjForUTCDate.js
+++ utils/bearcobble/adjForUTCDate.js
@@ -1,10 +1,16 @@
 function adjustForUTC(timeToConvert) {
     let currentHours = timeToConvert.getUTCHours();
     //adjust for DST vs STD
-    let hourDiff = new Date().getTimezoneOffset() / 60;
+    let hourDiff = 5;
+    let now = new Date()
+    let std = new Date(now.getFullYear(), 0, 1);
+    console.log("Offset calc: ", std.getTimezoneOffset(), now.getTimezoneOffset())
+    if (std.getTimezoneOffset() > now.getTimezoneOffset()) {
+        hourDiff = 4;
+    }
     console.log(hourDiff);
     timeToConvert.setUTCHours(currentHours - hourDiff);
     return timeToConvert;
 }
 
-module.exports = {adjustForUTC};
+module.exports = { adjustForUTC };
