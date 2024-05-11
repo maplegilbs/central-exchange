72de7289901cb73fca41328bf9a8c1813aa70076
Scott Gilbert <scottvtgilbert@gmail.com>
2024-05-11 19:27:51 -0400

    Adjust date for utc for heroku debug

--- utils/bearcobble/adjForUTCDate.js
+++ utils/bearcobble/adjForUTCDate.js
@@ -4,7 +4,8 @@ function adjustForUTC(timeToConvert) {
     let hourDiff = 5;
     let now = new Date()
     let std = new Date(now.getFullYear(), 0, 1);
-    console.log("Offset calc: ", std.getTimezoneOffset(), now.getTimezoneOffset())
+    let dst = new Date(now.getFullYear(), 6, 7);
+    console.log("Offset calc: ", std.getTimezoneOffset(), dst.getTimezoneOffset(), now.getTimezoneOffset())
     if (std.getTimezoneOffset() > now.getTimezoneOffset()) {
         hourDiff = 4;
     }
