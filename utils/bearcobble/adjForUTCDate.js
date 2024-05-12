5169554e07358c0031e567e50ce4bf7c966aafec
Scott Gilbert <scottvtgilbert@gmail.com>
2024-05-11 20:42:56 -0400

    Daylight savings update for UTC for use in heroku

--- utils/bearcobble/adjForUTCDate.js
+++ utils/bearcobble/adjForUTCDate.js
@@ -1,17 +1,16 @@
 function adjustForUTC(timeToConvert) {
     let currentHours = timeToConvert.getUTCHours();
     //adjust for DST vs STD
+    let intlDate = new Intl.DateTimeFormat('en-US', {
+        dateStyle: 'full',
+        timeStyle: 'long',
+        timeZone: 'America/New_York'
+    }).format(timeToConvert);
     let hourDiff = 5;
-    let now = new Date()
-    let std = new Date(now.getFullYear(), 0, 1);
-    let dst = new Date(now.getFullYear(), 6, 7);
-    let estString = std.toLocaleString('en-US', { timeZone: 'America/New_York' });
-    let estDate = new Date(estString);
-    console.log("Offset calc: ", std.getTimezoneOffset(), dst.getTimezoneOffset(), estDate.getTimezoneOffset())
-    if (std.getTimezoneOffset() > now.getTimezoneOffset()) {
-        hourDiff = 4;
+    if(intlDate.includes('EDT')){
+        hourDiff = 4
     }
-    console.log(hourDiff);
+    console.log(hourDiff)
     timeToConvert.setUTCHours(currentHours - hourDiff);
     return timeToConvert;
 }
