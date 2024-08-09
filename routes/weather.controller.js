df6b87d612bff98e9bf8adee88f9783f487fcfc8
Scott Gilbert <scottvtgilbert@gmail.com>
2024-08-09 10:16:52 -0400

    Adding gauge info route

--- routes/weather.controller.js
+++ routes/weather.controller.js
@@ -1,9 +1,18 @@
 const { handleError } = require('../utils/handleError');
 const router = require('express').Router();
 
-function createURL(source, lat, long) {
+async function createURL(source, lat, long) {
     if (source === 'openWeather') {
-        return `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&appid=${process.env.OW_APIKEY}`
+        return `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=imperial&appid=${process.env.OW_APIKEY}`
+    }
+    if (source === 'noaa') {
+        try {
+            let pointResponse = await fetch(`https://api.weather.gov/points/${lat},${long}`)
+            let pointJSON = await pointResponse.json()
+            return (pointJSON.properties.forecast)
+        } catch (error) {
+            return new Error(`There was an error creating the forecast URL: ${error}`)
+        }
     }
 }
 
@@ -11,9 +20,10 @@ router.get('/forecast', async (req, res) => {
     try {
         const { source, lat, long } = req.query;
         console.log(source, lat, long)
-        let forecastResponse = await fetch(createURL(source, lat, long))
+        let forecastURL = await createURL(source, lat, long)
+        let forecastResponse = await fetch(forecastURL)
         if (forecastResponse.status < 199 || forecastResponse.status > 300) {
-            throw new error(`Fetch to ${createURL(source, lat, long)} failed.`)
+            throw new Error(`Fetch to ${createURL(source, lat, long)} failed.`)
         }
         else {
             let forecastData = await forecastResponse.json()
