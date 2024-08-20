32b87cf7b92900dc80cace09d28cb8636c6476dc
Scott Gilbert <scottvtgilbert@gmail.com>
2024-08-19 20:50:38 -0400

    Adding gauges router to creekvtflows controller

--- routes/creekvtflows.controller.js
+++ routes/creekvtflows.controller.js
@@ -3,8 +3,10 @@ const router = require('express').Router();
 
 const levelsRouter = require('./creekvtflows/levelReports.controller.js');
 const rainRouter = require('./creekvtflows/rainInfo.controller.js');
+const gaugesRouter = require('./creekvtflows/gauges.controller.js');
 
 router.use('/levels', levelsRouter);
 router.use('/rain', rainRouter);
+router.use('/gauges', gaugesRouter)
 
 module.exports = router;
