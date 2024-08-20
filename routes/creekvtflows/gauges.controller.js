d0ccae6a81b6f06166901d4bceaaaa1c238f9433
Scott Gilbert <scottvtgilbert@gmail.com>
2024-08-19 20:49:50 -0400

    Adding gauges router

---
+++ routes/creekvtflows/gauges.controller.js
@@ -0,0 +1,23 @@
+//Libraries
+const router = require('express').Router();
+const mysql = require('mysql2')
+const {handleError} = require('../../utils/handleError.js')
+
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_ADMIN_USER,
+    password: process.env.DB_ADMIN_PASSWORD,
+    database: process.env.DB_NAME_CREEKVTFLOWS
+}).promise();
+
+
+//GET -- get a list of all gauges
+router.get("/", async(req, res)=>{
+    try {
+        let queryStatement = `select * from gauges`;
+        let results = await connection.query(queryStatement)
+        res.status(200).json(results[0])
+    } catch (error) {
+        handleError(error, req, res)
+    }
+})
