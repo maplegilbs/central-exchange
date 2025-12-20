6c893a7615c91fc8c9ce43f4e6e540ec47b8629d
Scott Gilbert <scottvtgilbert@gmail.com>
2025-12-19 22:54:01 -0500

    Updating tank and sensor array index references after putting new tablet into service and reinstalling sensors on smartrek application

--- utils/bearcobble/getSensorData.js
+++ utils/bearcobble/getSensorData.js
@@ -15,55 +15,55 @@ async function getSensorData() {
                 {
                     section1: {
                         section_name: 'Section 1',
-                        vacuum_reading: sensorJSON.database.table.row[0].data_EXT.PRESSURE+sensorJSON.database.table.row[0].configuration_EXT.CALIBZERO,
-                        reading_time: sensorJSON.database.table.row[0].timeTag_EXT.realtime
+                        vacuum_reading: sensorJSON.database.table.row[3].data_EXT.PRESSURE+sensorJSON.database.table.row[3].configuration_EXT.CALIBZERO,
+                        reading_time: sensorJSON.database.table.row[3].timeTag_EXT.realtime
                     },
                     section2: {
                         section_name: 'Section 2',
-                        vacuum_reading: sensorJSON.database.table.row[0].data_EXT.PRESSURE2+sensorJSON.database.table.row[0].configuration_EXT.CALIBZERO2,
-                        reading_time: sensorJSON.database.table.row[0].timeTag_EXT.realtime
+                        vacuum_reading: sensorJSON.database.table.row[3].data_EXT.PRESSURE2+sensorJSON.database.table.row[3].configuration_EXT.CALIBZERO2,
+                        reading_time: sensorJSON.database.table.row[3].timeTag_EXT.realtime
                     },
                     section3: {
                         section_name: 'Section 3',
-                        vacuum_reading: sensorJSON.database.table.row[1].data_EXT.PRESSURE2+sensorJSON.database.table.row[1].configuration_EXT.CALIBZERO2,
-                        reading_time: sensorJSON.database.table.row[1].timeTag_EXT.realtime
+                        vacuum_reading: sensorJSON.database.table.row[2].data_EXT.PRESSURE2+sensorJSON.database.table.row[2].configuration_EXT.CALIBZERO2,
+                        reading_time: sensorJSON.database.table.row[2].timeTag_EXT.realtime
                     },
                     section4: {
                         section_name: 'Section 4',
-                        vacuum_reading: sensorJSON.database.table.row[2].data_EXT.PRESSURE+sensorJSON.database.table.row[2].configuration_EXT.CALIBZERO,
-                        reading_time: sensorJSON.database.table.row[2].timeTag_EXT.realtime
+                        vacuum_reading: sensorJSON.database.table.row[0].data_EXT.PRESSURE+sensorJSON.database.table.row[0].configuration_EXT.CALIBZERO,
+                        reading_time: sensorJSON.database.table.row[0].timeTag_EXT.realtime
                     },
                     section5: {
                         section_name: 'Section 5',
-                        vacuum_reading: sensorJSON.database.table.row[2].data_EXT.PRESSURE2+sensorJSON.database.table.row[2].configuration_EXT.CALIBZERO2,
-                        reading_time: sensorJSON.database.table.row[2].timeTag_EXT.realtime
+                        vacuum_reading: sensorJSON.database.table.row[0].data_EXT.PRESSURE2+sensorJSON.database.table.row[0].configuration_EXT.CALIBZERO2,
+                        reading_time: sensorJSON.database.table.row[0].timeTag_EXT.realtime
                     },
                 },
                 {
                     tank1: {
                         tank_name: 'Tank 1',
-                        tank_level: sensorJSON.database.table.row[3].data_1,
-                        reading_time: sensorJSON.database.table.row[3].timeTag_EXT.realtime
-                    },
-                    tank2: {
-                        tank_name: 'Tank 2',
-                        tank_level: sensorJSON.database.table.row[8].data_1,
-                        reading_time: sensorJSON.database.table.row[8].timeTag_EXT.realtime
-                    },
-                    tank3: {
-                        tank_name: 'Tank 3',
                         tank_level: sensorJSON.database.table.row[4].data_1,
                         reading_time: sensorJSON.database.table.row[4].timeTag_EXT.realtime
                     },
-                    tank4: {
-                        tank_name: 'Tank 4',
+                    tank2: {
+                        tank_name: 'Tank 2',
                         tank_level: sensorJSON.database.table.row[5].data_1,
                         reading_time: sensorJSON.database.table.row[5].timeTag_EXT.realtime
                     },
-                    tank5: {
-                        tank_name: 'Tank 5',
+                    tank3: {
+                        tank_name: 'Tank 3',
                         tank_level: sensorJSON.database.table.row[6].data_1,
                         reading_time: sensorJSON.database.table.row[6].timeTag_EXT.realtime
+                    },
+                    tank4: {
+                        tank_name: 'Tank 4',
+                        tank_level: sensorJSON.database.table.row[7].data_1,
+                        reading_time: sensorJSON.database.table.row[7].timeTag_EXT.realtime
+                    },
+                    tank5: {
+                        tank_name: 'Tank 5',
+                        tank_level: sensorJSON.database.table.row[8].data_1,
+                        reading_time: sensorJSON.database.table.row[8].timeTag_EXT.realtime
                     }
                 },
                 fetchedWeatherJSON['observations']['0']['imperial']['pressure']
